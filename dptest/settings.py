a043552a2776a7ce5219ec329f368ad664a3cab8
fengb <benjamin.feng@gmail.com>
2011-12-06 09:04:15 -0600

    Added ugly hack to use app outside of project proper.

--- dptest/settings.py
+++ dptest/settings.py
@@ -1,5 +1,10 @@
 # Django settings for dptest project.
 
+# Evil hack!
+import sys
+import os
+sys.path.append(os.path.dirname(sys.path[0]))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,7 +16,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -115,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pyort',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
