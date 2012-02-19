3a6613110f056376e8b6a724c781665ea79a2f30
fengb <benjamin.feng@gmail.com>
2012-02-18 22:38:22 -0600

    Updated pyort models to be specifically used for transactions.

--- pyort/models.py
+++ pyort/models.py
@@ -5,19 +5,13 @@ class Investment(models.Model):
     symbol          = models.CharField(max_length=5, unique=True)
     name            = models.CharField(max_length=200)
 
+    class Admin(object): pass
 
-class Snapshot(models.Model):
+
+class Transaction(models.Model):
     investment      = models.ForeignKey(Investment)
     date            = models.DateField()
     price           = models.DecimalField(max_digits=12, decimal_places=4)
-
-
-class Portfolio(models.Model):
-    name            = models.CharField(max_length=200)
-
-
-class Holding(models.Model):
-    portfolio       = models.ForeignKey(Portfolio)
-    snapshot        = models.ForeignKey(Snapshot)
-
     shares          = models.DecimalField(max_digits=15, decimal_places=4)
+
+    class Admin(object): pass
