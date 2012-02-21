06bec3c19689cf1116c58d788b7c93af63f9c4d7
fengb <benjamin.feng@gmail.com>
2012-02-21 09:54:56 -0600

    Added trailing slash.

--- pyort/urls.py
+++ pyort/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^portfolio/(?P<id>\d+)$', views.portfolio),
-    url(r'^portfolio/(?P<id>\d+)/basis$', views.portfolio_basis),
+    url(r'^portfolio/(?P<id>\d+)/$', views.portfolio),
+    url(r'^portfolio/(?P<id>\d+)/basis/$', views.portfolio_basis),
 )
