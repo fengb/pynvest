e291efb50a63bee2f6c8ab89526a86ab21835bae
fengb <benjamin.feng@gmail.com>
2012-02-26 00:19:26 -0600

    Refactored TransactionAggregate logic

--- pyort/models/core.py
+++ pyort/models/core.py
@@ -1,3 +1,8 @@
+from .. import util
+from . import Portfolio, Lot, Transaction
+import operator
+
+
 class TransactionAggregate(object):
     def __init__(self, transactions):
         self.transactions = list(transactions)
@@ -25,3 +30,13 @@ class TransactionAggregate(object):
         return sum(transaction.shares
                      for transaction in self.transactions)
 
+    @classmethod
+    def for_portfolio_by_investment(cls, portfolio):
+        transactions = Transaction.objects.filter(lot__portfolio=portfolio)
+        grouped = util.groupbyrollup(transactions, key=operator.attrgetter('investment'))
+        return [cls(v) for (k, v) in grouped]
+
+    @classmethod
+    def for_portfolio_by_lot(cls, portfolio):
+        return [cls(lot.transaction_set.all())
+                 for lot in Lot.objects.filter(portfolio=portfolio)]
--- pyort/views.py
+++ pyort/views.py
@@ -5,27 +5,20 @@ from . import util
 import operator
 
 
-def _render_transactions_grouped(transactions_grouped, portfolio=None):
-    return render_to_response('pyort/transaction_aggregates.html', {
-        'portfolio': portfolio,
-        'transaction_aggregates': [models.TransactionAggregate(v)
-                                    for (k, v) in transactions_grouped],
-    })
-
-
 def portfolio(request, id):
     portfolio = get_object_or_404(models.Portfolio, id=id)
-
-    transactions = models.Transaction.objects.filter(lot__portfolio=portfolio)
-    grouped = util.groupbyrollup(transactions, key=operator.attrgetter('investment'))
-
-    return _render_transactions_grouped(grouped)
+    return render_to_response('pyort/transaction_aggregates.html', {
+        'title': portfolio.name,
+        'transaction_aggregates': models.TransactionAggregate.for_portfolio_by_investment(portfolio),
+    })
 
 
 def portfolio_lot(request, id):
     portfolio = get_object_or_404(models.Portfolio, id=id)
-    return _render_transactions_grouped((lot, lot.transaction_set.all())
-                                          for lot in portfolio.lot_set.all())
+    return render_to_response('pyort/transaction_aggregates.html', {
+        'title': portfolio.name,
+        'transaction_aggregates': models.TransactionAggregate.for_portfolio_by_lot(portfolio),
+    })
 
 def portfolio_flat(request, id):
     portfolio = get_object_or_404(models.Portfolio, id=id)
