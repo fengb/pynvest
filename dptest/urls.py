8d50b3a26cd1b2d6aee61a987c7e37c13cc642a4
fengb <benjamin.feng@gmail.com>
2012-03-09 22:39:32 -0600

    Fixed URLs

--- dptest/urls.py
+++ dptest/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^pyort/', include('pyort.urls')),
+    url(r'^pynvest/portfolio/', include('pynvest_portfolio.urls')),
 )
--- pynvest_portfolio/urls.py
+++ pynvest_portfolio/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^portfolio/(?P<id>\d+)/$', views.portfolio),
-    url(r'^portfolio/(?P<id>\d+)/lot/$', views.portfolio_summary),
-    url(r'^portfolio/(?P<id>\d+)/lot/(?P<year>\d+)/$', views.portfolio_summary),
-    url(r'^portfolio/(?P<id>\d+)/(?P<year>\d+)/sales$', views.portfolio_sales_by_year),
-    url(r'^portfolio/(?P<id>\d+)/flat/$', views.portfolio_flat),
+    url(r'^(?P<id>\d+)/$', views.portfolio),
+    url(r'^(?P<id>\d+)/lot/$', views.portfolio_summary),
+    url(r'^(?P<id>\d+)/lot/(?P<year>\d+)/$', views.portfolio_summary),
+    url(r'^(?P<id>\d+)/(?P<year>\d+)/sales$', views.portfolio_sales_by_year),
+    url(r'^(?P<id>\d+)/flat/$', views.portfolio_flat),
 )
