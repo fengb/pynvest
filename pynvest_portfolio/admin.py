96ffb65bff957ce9ea3506e8508daa3d912518be
fengb <benjamin.feng@gmail.com>
2012-03-19 22:18:34 -0500

    Added LotTransaction to admin

--- pynvest_portfolio/admin.py
+++ pynvest_portfolio/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register([models.Portfolio, models.Lot, models.Transaction])
+admin.site.register([models.Portfolio, models.Lot, models.Transaction, models.LotTransaction])
