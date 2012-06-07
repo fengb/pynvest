a531fe2ad7da12af2faa3b0761f3f0f2b98689fe
fengb <benjamin.feng@gmail.com>
2012-06-07 18:16:02 -0500

    Added exchange information to investments

--- pynvest_core/admin.py
+++ pynvest_core/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register([models.Investment])
+admin.site.register([models.Exchange, models.Investment])
--- pynvest_core/models.py
+++ pynvest_core/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
-class Investment(models.Model):
-    symbol          = models.CharField(max_length=5, unique=True)
+class Exchange(models.Model):
+    symbol          = models.CharField(max_length=10, unique=True)
     name            = models.CharField(max_length=200)
 
     def __unicode__(self):
         return u'%s' % self.symbol
+
+class Investment(models.Model):
+    exchange        = models.ForeignKey(Exchange)
+    symbol          = models.CharField(max_length=10, unique=True)
+    name            = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return u'%s' % (self.symbol)
