32658c52cca7e5b64d6f10c9f924eb6b632cb5a3
fengb <benjamin.feng@gmail.com>
2012-06-20 22:49:34 -0500

    Extended pynvest_connect.historical_prices API to limit by date

--- pynvest_connect/yahoo.py
+++ pynvest_connect/yahoo.py
@@ -11,8 +11,24 @@ def _convert(field):
         return decimal.Decimal(field)
 
 
-def historical_prices(symbol):
-    response = urllib2.urlopen('http://ichart.finance.yahoo.com/table.csv?s=%s' % symbol)
+def historical_prices(symbol, start_date=None, end_date=None):
+    # Yahoo API is really terrible...
+    params = ['s=%s' % symbol]
+    if start_date:
+        params.extend([
+            # POSIX date means January=0, February=1, etc.
+            'a=%d' % (start_date.month - 1),
+            'b=%d' % start_date.day,
+            'c=%d' % start_date.year,
+        ])
+    if end_date:
+        params.extend([
+            'd=%d' % (end_date.month - 1),
+            'e=%d' % end_date.day,
+            'f=%d' % end_date.year,
+        ])
+
+    response = urllib2.urlopen('http://ichart.finance.yahoo.com/table.csv?' + '&'.join(params))
     try:
         raw = csv.reader(response)
 
