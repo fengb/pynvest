0c84e7c3f0a1c0ee7f6a4216f2ae55acce942ece
fengb <benjamin.feng@gmail.com>
2012-07-17 22:46:28 -0500

    Added preliminary Google API

---
+++ pynvest_connect/google.py
@@ -0,0 +1,31 @@
+import urllib2
+import collections
+import csv
+import re
+import datetime
+import decimal
+
+
+def convert_string(string):
+    if '-' in string:
+        return datetime.datetime.strptime(string, '%d-%b-%y')
+    else:
+        return decimal.Decimal(string)
+
+
+_DIRECTIVES_PATTERN = re.compile('[\W_]+')
+def historical_prices(symbol, start_date=None, end_date=None):
+    params = ['q=%s' % symbol, 'output=csv']
+    if start_date:
+        params.append(start_date.strftime('startdate=%b+%d,+%Y'))
+    if end_date:
+        params.append(start_date.strftime('startdate=%b+%d,+%Y'))
+
+    response = urllib2.urlopen('http://www.google.com/finance/historical?' + '&'.join(params))
+    try:
+        raw = csv.reader(response)
+
+        tuple = collections.namedtuple('HistoricalPrice', [_DIRECTIVES_PATTERN.sub('', directive.lower()) for directive in next(raw)])
+        return [tuple(*map(convert_string, row)) for row in raw]
+    finally:
+        response.close()
--- pynvest_connect/util.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import decimal
-
-
-def convert_string(field):
-    if '-' in field:
-        return datetime.date(*map(int, field.split('-')))
-    else:
-        return decimal.Decimal(field)
