48f421e5ca51052408370a573928f54a2f257c02
fengb <benjamin.feng@gmail.com>
2012-07-22 19:23:24 -0500

    Limited price display to a year

--- pynvest_core/urls.py
+++ pynvest_core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^investment/(?P<symbol>\S+)/$', views.investment_snapshots),
-    url(r'^investment/(?P<symbol>\S+)/growth$', views.investment_growth),
+    url(r'^investment/(?P<symbol>\w+)/((?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/)?$', views.investment_snapshots),
+    url(r'^investment/(?P<symbol>\w+)/growth/$', views.investment_growth),
 )
--- pynvest_core/views.py
+++ pynvest_core/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from . import models, presenters
+import datetime
 import decimal
 
 
-def investment_snapshots(request, symbol):
+def investment_snapshots(request, symbol, year=None, month=None, day=None):
     investment = get_object_or_404(models.Investment, symbol=symbol)
+    end_date = datetime.date(int(year), int(month), int(day)) if year else datetime.date.today()
+    start_date = end_date - datetime.timedelta(days=365)
 
     return render_to_response('pynvest_core/snapshots_table.html', {
         'title': investment.symbol,
-        'snapshots': investment.snapshot_set.order_by('-date'),
+        'snapshots': investment.snapshot_set.filter(date__gte=start_date, date__lte=end_date).order_by('-date'),
     })
 
 
