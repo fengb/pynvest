e1c819b79b326db1d419f065e465f7649538277f
fengb <benjamin.feng@gmail.com>
2012-07-29 20:46:49 -0500

    Added API doc on the new hook

--- pynvest_core/managers.py
+++ pynvest_core/managers.py
@@ -17,6 +17,23 @@ class QuerySetManager(models.Manager):
     >>> [e.data for e in Entry.objects.why()]
     ['Jay', 'Fry']
 
+    There is a hook 'from_manager' that will replace the standard constructor if
+    it exists.  This will automatically run whenever get_query_set() is called.
+
+    >>> class Entry(models.Model):
+    ...     data = models.CharField()
+    ...     objects = QuerySetManager()
+    ...     class QuerySet(models.query.QuerySet):
+    ...         @classmethod
+    ...         def from_manager(cls, *args, **kwargs):
+    ...             return cls(*args, **kwargs).filter(data__like='y').order_by('data')
+
+    >>> Entry.create(data='Philip')
+    >>> Entry.create(data='Jay')
+    >>> Entry.create(data='Fry')
+    >>> [e.data for e in Entry.objects.all()]
+    ['Jay', 'Fry']
+
     http://djangosnippets.org/snippets/734/'''
 
     use_for_related_fields = True
