aae3f49e774c0209c6a75bbec964358f9570171e
fengb <benjamin.feng@gmail.com>
2012-08-02 16:20:51 -0500

    Removed investment growth.  Not useful by itself...

--- pynvest_core/views.py
+++ pynvest_core/views.py
@@ -21,13 +21,3 @@ def investment_snapshots(request, symbol, year=None, month=None, day=None):
         'title': investment.symbol,
         'snapshots': investment.snapshot_set.filter_year_range(end_date=end_date).order_by('-date'),
     })
-
-
-def investment_growth(request, symbol):
-    investment = get_object_or_404(models.Investment, symbol=symbol)
-
-    return render_to_response('pynvest_core/growths_table.html', {
-        'title': investment.symbol,
-        'growths': [presenters.InvestmentGrowth.lump_sum(investment, start_date=investment.snapshot_set.order_by('date')[0].date,
-                                                                     start_value=decimal.Decimal(10000))]
-    })
