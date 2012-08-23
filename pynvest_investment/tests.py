3e7ec6c317d36a1b58bb9c32140299ea7a329844
fengb <benjamin.feng@gmail.com>
2012-08-22 20:59:02 -0500

    Refactored test data generation

--- pynvest_investment/tests.py
+++ pynvest_investment/tests.py
@@ -10,16 +10,21 @@ class TestSnapshot(TestCase):
         exchange = models.Exchange.objects.create(symbol='a', name='b')
         self.investment = models.Investment.objects.create(exchange=exchange, symbol='c', name='d')
 
+    def create_snapshot(self, **kwargs):
+        data = {'investment': self.investment, 'high': 100, 'low': 1, 'close': 10, 'date': datetime.date.today()}
+        data.update(kwargs)
+        return models.Snapshot.objects.create(**data)
+
     def test_dividend_percent(self):
-        snapshot = models.Snapshot(dividend=decimal.Decimal('0.2'), close=20)
+        snapshot = self.create_snapshot(dividend=decimal.Decimal('0.2'), close=20)
         self.assertEquals(snapshot.dividend_percent(), decimal.Decimal('0.01'))
 
     def test_split(self):
-        snapshot = models.Snapshot(split_before=5, split_after=9)
+        snapshot = self.create_snapshot(split_before=5, split_after=9)
         self.assertEquals(snapshot.split(), '9:5')
 
     def test_close_adjusted_base(self):
-        models.Snapshot.objects.create(investment=self.investment, high=5, low=3, close=4, date=datetime.date.today())
+        self.create_snapshot(close=4)
 
         snapshot = models.Snapshot.objects.close_adjusted()[0]
         self.assertEquals(snapshot.close_adjusted, 4)
