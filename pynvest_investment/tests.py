60cfa5ba948ec1d952bd2a0ec8356af1b0286e12
fengb <benjamin.feng@gmail.com>
2012-08-22 20:50:37 -0500

    Added basic tests for Snapshot

--- pynvest_investment/tests.py
+++ pynvest_investment/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from pynvest_investment import models
+
+import decimal
+import datetime
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestSnapshot(TestCase):
+    def setUp(self):
+        exchange = models.Exchange.objects.create(symbol='a', name='b')
+        self.investment = models.Investment.objects.create(exchange=exchange, symbol='c', name='d')
+
+    def test_dividend_percent(self):
+        snapshot = models.Snapshot(dividend=decimal.Decimal('0.2'), close=20)
+        self.assertEquals(snapshot.dividend_percent(), decimal.Decimal('0.01'))
+
+    def test_split(self):
+        snapshot = models.Snapshot(split_before=5, split_after=9)
+        self.assertEquals(snapshot.split(), '9:5')
+
+    def test_close_adjusted_base(self):
+        models.Snapshot.objects.create(investment=self.investment, high=5, low=3, close=4, date=datetime.date.today())
+
+        snapshot = models.Snapshot.objects.close_adjusted()[0]
+        self.assertEquals(snapshot.close_adjusted, 4)
