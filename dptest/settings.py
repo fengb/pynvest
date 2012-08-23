5d6fc12e40364e303317f8aaf2ddbeeee9da56db
fengb <benjamin.feng@gmail.com>
2012-08-22 20:40:04 -0500

    Removed unused dependencies

--- dptest/settings.py
+++ dptest/settings.py
@@ -112,8 +112,8 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
+    #'django.contrib.sites',
+    #'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
