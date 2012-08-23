7ee0097963742ddb21e77f97d20e35925dda1fa3
fengb <benjamin.feng@gmail.com>
2012-08-23 08:36:31 -0500

    Added navigation links

--- pynvest_investment/views.py
+++ pynvest_investment/views.py
@@ -4,22 +4,30 @@ import datetime
 import decimal
 
 
+def current_year():
+    return datetime.date.today().year
+
 def investment(request, symbol):
     investment = get_object_or_404(models.Investment, symbol=symbol)
 
     return render_to_response('pynvest_investment/investment.html', {
         'title': investment.symbol,
         'investment': investment,
-        'year': datetime.date.today().year,
+        'year': current_year(),
     })
 
 
-def investment_snapshots(request, symbol, year=None):
+def investment_snapshots(request, symbol, year):
     investment = get_object_or_404(models.Investment, symbol=symbol)
+    year = int(year)
 
-    return render_to_response('pynvest_investment/snapshots_table.html', {
+    vars = {
         'title': investment.symbol,
+        'investment': investment,
         'snapshots': investment.snapshot_set.filter(date__year=year
                                            ).close_adjusted(
                                            ).order_by('-date'),
-    })
+    }
+    vars['prev_year'] = year - 1 if investment.snapshot_set.filter(date__year=(year-1)).exists() else None
+    vars['next_year'] = year + 1 if year < current_year() else None
+    return render_to_response('pynvest_investment/snapshots_table.html', vars)
