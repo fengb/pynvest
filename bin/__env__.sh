7b25db2ee2d86cb72de7f9e11c82025abb3c07e2
fengb <benjamin.feng@gmail.com>
2012-08-31 09:29:41 -0500

    Extracted out environment setup

---
+++ bin/__env__.sh
@@ -0,0 +1,5 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export SRC_DIR=$SCRIPT_DIR/..
+export PROJECT_DIR=$SRC_DIR/dptest
+
+cd $SRC_DIR
--- bin/tests.sh
+++ bin/tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_DIR="$SCRIPT_DIR/.."
+cd $SCRIPT_DIR
+. __env__.sh
 
-cd $PROJECT_DIR
-mkdir -p $PROJECT_DIR/test
+
+mkdir -p test
 export DJANGO_SETTINGS_MODULE=dptest.settings_test
-$PROJECT_DIR/dptest/manage.py flush --noinput --settings=dptest.settings_test
+dptest/manage.py flush --noinput --settings=dptest.settings_test
 py.test $*
