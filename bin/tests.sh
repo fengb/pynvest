6fb6de9856895b70d634b9a96e20200b4b790583
fengb <benjamin.feng@gmail.com>
2012-12-03 23:54:19 -0600

    Updated test script

--- bin/tests.sh
+++ bin/tests.sh
@@ -6,7 +6,8 @@ export PROJECT_DIR=$PYTHONPATH/dptest
 cd $PYTHONPATH
 
 
+rm -rf test
 mkdir -p test
 export DJANGO_SETTINGS_MODULE=dptest.settings_test
-dptest/manage.py flush --noinput --settings=dptest.settings_test
+dptest/manage.py syncdb --noinput --settings=dptest.settings_test
 py.test $*
