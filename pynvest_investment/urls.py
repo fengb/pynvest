a1f3f28a5592060ee034ce6b0cf50fdb41ef9ce0
fengb <benjamin.feng@gmail.com>
2012-12-21 01:22:11 -0600

    Modified investment URL to account for arbitrary characters

--- pynvest_investment/urls.py
+++ pynvest_investment/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('pynvest_investment.views',
-    url(r'^(?P<symbol>\w+)/$',                       'summary'),
-    url(r'^(?P<symbol>\w+)/price/(?P<year>\d{4})/$', 'snapshots'),
+    url(r'^(?P<symbol>[^/]+)/$',                       'summary'),
+    url(r'^(?P<symbol>[^/]+)/price/(?P<year>\d{4})/$', 'snapshots'),
 )
