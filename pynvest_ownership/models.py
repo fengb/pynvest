ee1b3760860402920762db626c3cf42c4319a811
fengb <benjamin.feng@gmail.com>
2013-05-03 18:43:07 -0500

    Added helpers for easier contribution processing

--- pynvest_ownership/models.py
+++ pynvest_ownership/models.py
@@ -18,7 +18,20 @@ class Contribution(models.Model):
     def total_units(cls, portfolio, date):
         return sum(Contribution.objects.filter(portfolio=portfolio, date__lte=date).values_list('units', flat=True))
 
+    @classmethod
+    def amount_at(cls, portfolio, date):
+        return pynvest_portfolio.presenters.PortfolioGrowth(portfolio)[date]
+
+    @classmethod
+    def units_for_new_amount(cls, portfolio, date, amount):
+        return amount * cls.total_units(portfolio, date) / \
+               (cls.amount_at(portfolio, date) - amount)
+
+    @classmethod
+    def new(cls, user, portfolio, date, amount):
+        return cls(user=user, portfolio=portfolio, date=date,
+                   units=cls.units_for_new_amount(portfolio, date, amount))
+
     def amount(self):
-        growth = pynvest_portfolio.presenters.PortfolioGrowth(self.portfolio)
         percentage = self.units / self.total_units(self.portfolio, self.date)
-        return percentage * growth[self.date]
+        return percentage * self.amount_at(self.portfolio, self.date)
